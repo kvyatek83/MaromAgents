b5ab0cdef31563a713965de12fd7e0d15afc58f1
Netanel Kvyatek <kvyatek83@gmail.com>
2018-04-23 17:18:04 +0300

    Added product list on mens and ladies perfums pages.

--- angular-src/src/app/product-pages/mens-perfume-page/mens-perfume-page.component.ts
+++ angular-src/src/app/product-pages/mens-perfume-page/mens-perfume-page.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ProductsService } from '../../products.service';
+
 @Component({
   selector: 'app-mens-perfume-page',
   templateUrl: './mens-perfume-page.component.html',
   styleUrls: ['./mens-perfume-page.component.css']
 })
 export class MensPerfumePageComponent implements OnInit {
+  
+  mens : any;
 
-  constructor() { }
+  constructor(private productsService: ProductsService) { }
 
   ngOnInit() {
+    this.getAllMensPerfumeProducts();
+  }
+
+  getAllMensPerfumeProducts(){
+    this.productsService.getProductsByCategoryAndGender("בושם", "גבר").subscribe(
+      data => {this.mens = data},
+      err => console.error(err));
   }
 
 }
